a7461b1cb477f7a57d880fb8adf3ee931fbf80e5
whw <whw.0922@gmail.com>
2024-06-16 13:07:43 +0800

    remove line breaks

--- hammerspoon/.hammerspoon/modules/audio.lua
+++ hammerspoon/.hammerspoon/modules/audio.lua
@@ -1,5 +1,5 @@
 hs.hotkey.bind({ "cmd", "shift" }, "b", function()
 	hs.execute("/opt/homebrew/bin/SwitchAudioSource -n")
 	local output = hs.execute("/opt/homebrew/bin/SwitchAudioSource -c")
-	hs.alert.show("switch to " .. output)
+	hs.alert.show("switch to " .. output:match("[^\n]*"))
 end)
